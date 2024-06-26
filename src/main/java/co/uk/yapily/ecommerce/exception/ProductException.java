92a2d797fa25462bfbcb1aea0500df7af0a87eb6
Bruna Vita <brunavita@gmail.com>
2024-06-26 17:59:47 +0100

    Delete src directory

--- src/main/java/co/uk/yapily/ecommerce/exception/ProductException.java
+++
@@ -1,7 +0,0 @@
-package co.uk.yapily.ecommerce.exception;
-
-public class ProductException extends RuntimeException {
-    public ProductException(String message) {
-        super(message);
-    }
-}
